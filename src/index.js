b5d5ead82d1181a5376425935c393d9704e2fc6e
Zhenya84 <55429535+Zhenya84@users.noreply.github.com>
2020-10-05 17:10:51 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ module.exports = function reverse (n) {
   let result = '';
   if (n === 0) return 0;
   if (n < 0) return reverse(-n);
-  while (n > 0){
-    if (n%10 != 0) {
-        result = result + (n%10);
-        n = parseInt(n/10);
-    } else n = parseInt(n/10);
-  }
+  if (n%10 != 0) {
+   while (n > 0) {
+     result = result + (n%10);
+     n = parseInt(n/10);
+   }
+  } else {
+     n = parseInt(n/10);
+     while (n > 0) {
+     result = result + (n%10);
+     n = parseInt(n/10);
+     }
+    }
   return result;
 }
