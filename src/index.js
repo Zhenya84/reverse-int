0b8ddeee35b6871efc2b26e7d330a7ad7397aa5d
Zhenya84 <55429535+Zhenya84@users.noreply.github.com>
2020-10-05 16:50:18 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 module.exports = function reverse (n) {
   let result = '';
   if (number === 0) return 0;
-  if (number < 0) return -reverseInt(-number);
+  if (number < 0) return -reverse(-number);
   while (number > 0){
    result = result + (number%10);
    number = parseInt(number/10);
